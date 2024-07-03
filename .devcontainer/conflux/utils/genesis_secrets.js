303b82cbaaf7a43fd884dfef3d75f591196d781c
SP <spcfxda@gmail.com>
2024-07-03 09:52:22 +0200

    .devcontainer setup
    Two stage Dockerfile, for devcontainer, or standalone openvscode-server
    Utils for genesis private keys generation with js-conflux-sdk
    openvscode-server start script

---
+++ .devcontainer/conflux/utils/genesis_secrets.js
@@ -0,0 +1,32 @@
+const TOML = require('@iarna/toml');
+const fs = require('fs');
+const { PrivateKeyAccount: Account } = require('js-conflux-sdk');
+const configPath = process.env.CONFIG_PATH || "/opt/conflux/develop.toml";
+
+function genesisSecrets() {
+    const configString = fs.readFileSync(configPath, "utf-8");
+    const config = TOML.parse(configString);
+
+    let secrets = [];
+    
+    for (let i = 0; i < 5; i++) {
+      const randomAccount = Account.random(undefined, config.chain_id);
+      secrets.push(randomAccount.privateKey.replace('0x', ''));
+    }
+    // for (let i = 0; i < 5; i++) {
+    //   const randomAccount = Account.random(undefined, config.evm_chain_id);
+    //   secrets.push(randomAccount.privateKey.replace('0x', ''));
+    // }
+    secrets.push("")
+
+    fs.appendFile(config.genesis_secrets, secrets.join('\n'), (err) => {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('Secrets generated successfully!');
+      }
+    });
+}
+
+genesisSecrets();
+
