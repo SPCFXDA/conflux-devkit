b94dff13764418141074638797469737f1128285
SP <spcfxda@gmail.com>
2024-07-09 14:12:35 +0000

    eslint config update

--- .devcontainer/conflux/utils/eslint.config.mjs
+++ .devcontainer/conflux/utils/eslint.config.mjs
@@ -1,9 +1,20 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      sourceType: "commonjs",
+      globals: {
+        ...globals.node,
+      },
+    },
+  },
+  {
+    languageOptions: {
+      globals: globals.browser,
+    },
+  },
   pluginJs.configs.recommended,
 ];
